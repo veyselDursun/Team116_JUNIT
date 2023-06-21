eb23acb995197b180f2674532f8eb97a4ef1a596
veyselDursun <veysdursun@yahoo.com>
2023-06-21 09:40:37 +0200

    day07TestBase

---
+++ src/test/java/tests/day07_testBase_dropdown/C01_TestBaseIlkTest.java
@@ -0,0 +1,13 @@
+package tests.day07_testBase_dropdown;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C01_TestBaseIlkTest  extends TestBase {
+
+    @Test
+    public void test01(){
+
+
+    }
+}
